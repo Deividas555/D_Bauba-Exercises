2017fd20f9d5b6eb45ee28c623a35b5d311141e8
Deividas555 <deividasbaub@gmail.com>
2023-10-10 10:14:59 +0300

    4

---
+++ PD4/js/main.js
@@ -0,0 +1,11 @@
+let petName = "Gaidosas";
+let petAge = 16;
+
+console.log(petName, petAge);
+
+petName = "Gaidys"
+petAge = 15;
+
+const buildingAdress = "Giedraiciu g. 5"
+
+console.log(buildingAdress);
